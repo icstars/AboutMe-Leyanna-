34603acd8d566144f1a31f7b6e230479362d4b98
Lshackles <136016170+Lshackles@users.noreply.github.com>
2023-06-21 19:49:05 -0500

    fdasasasasdas
    asdasdasd

---
+++ index.js
@@ -0,0 +1,31 @@
+//VARIABLES
+//let,var,const
+
+//console.log (name); // Print to the console
+
+const name = "pizza";
+const topping = "pineapple";
+/* adding two stings;
+concatenation */
+
+console.log(name, topping);
+
+// TEMPLATE LITERALS
+console.log(`${name} ${topping}`);
+
+console.log("string" + 32);
+
+// integer
+
+// booleans
+
+//false, undefined, null, o
+
+let num = undefined;
+
+console.log(Boolean(num));
+
+let prompt1 = prompt("What is your name?");
+let howOldAreYou = prompt("How old are you?");
+let whatIsYourHobby = prompt("What is your Hobby?");
+alert(`${prompt1} is ${howOldAreYou} and likes ${whatIsYourHobby}`);
